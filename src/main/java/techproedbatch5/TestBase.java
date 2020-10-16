43568c4bd3fb21b8f426d715cec434674338c993
ahmetaydemir-nl <ahmetaydemir275@gmail.com>
2020-10-16 22:23:49 +0200

    API

---
+++ src/main/java/techproedbatch5/TestBase.java
@@ -0,0 +1,38 @@
+package techproedbatch5;
+
+
+import io.restassured.specification.RequestSpecification;
+import io.restassured.builder.RequestSpecBuilder;
+import org.junit.Before;
+
+public class TestBase {
+
+    protected RequestSpecification spec01; //cocuklarin  hepsi kullanabilsin diye
+    protected RequestSpecification spec02;
+
+    @Before
+    public void setUp01(){
+        spec01=  new  RequestSpecBuilder().
+                setBaseUri("https://restful-booker.herokuapp.com").
+                build();
+        //Uri olusturduk ve spec01 icerisine depoladik.
+        // Amacimiz kod tekrarindan kurtulma ve guncellemeleri daha hizli yapabilmek.
+
+    }
+    @Before
+    public void setUp02(){
+        spec02 = new  RequestSpecBuilder().
+                setBaseUri("http://dummy.restapiexample.com/api/v1/employees").
+                build();
+
+    }
+}
+
+
+
+
+
+
+
+
+
