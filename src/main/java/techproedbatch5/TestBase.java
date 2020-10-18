08d9efd5813f8ec4968c3e0b8155c7a21aeb4aa1
ahmetaydemir-nl <ahmetaydemir275@gmail.com>
2020-10-18 22:23:57 +0200

    get son

--- src/main/java/techproedbatch5/TestBase.java
+++ src/main/java/techproedbatch5/TestBase.java
@@ -9,26 +9,34 @@ public class TestBase {
 
     protected RequestSpecification spec01; //cocuklarin  hepsi kullanabilsin diye
     protected RequestSpecification spec02;
+    protected RequestSpecification spec03;
 
     @Before
-    public void setUp01(){
-        spec01=  new  RequestSpecBuilder().
+    public void setUp01() {
+        spec01 = new RequestSpecBuilder().
                 setBaseUri("https://restful-booker.herokuapp.com").
                 build();
         //Uri olusturduk ve spec01 icerisine depoladik.
         // Amacimiz kod tekrarindan kurtulma ve guncellemeleri daha hizli yapabilmek.
 
     }
+
     @Before
-    public void setUp02(){
-        spec02 = new  RequestSpecBuilder().
+    public void setUp02() {
+        spec02 = new RequestSpecBuilder().
                 setBaseUri("http://dummy.restapiexample.com/api/v1/employees").
                 build();
 
     }
+
+    @Before
+    public void setUp03() {
+        spec03 = new RequestSpecBuilder().
+                setBaseUri("https://jsonplaceholder.typicode.com/todos").
+                build();
+    }
+
 }
-
-
 
 
 
